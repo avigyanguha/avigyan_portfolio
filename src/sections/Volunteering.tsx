948a065b223d1788d82b120f31875e3002469bde
avigyan guha <avigyanguha427@gmail.com>
2026-09-18 19:03:09 +0530

    init volunteering section

---
+++ src/sections/Volunteering.tsx
@@ -0,0 +1,11 @@
+import "../styles/sections/Volunteering.css";
+
+function Volunteering() {
+  return (
+    <section id="volunteering" className="Volunteering">
+      Volunteering
+    </section>
+  );
+}
+
+export default Volunteering;
