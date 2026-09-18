f03312585f6de0bbfcd7c3ab14e915d18e70e49e
avigyan guha <avigyanguha427@gmail.com>
2026-09-18 19:04:52 +0530

    chn

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,9 @@ import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Skills from "./sections/Skills";
 import Projects from "./sections/Projects";
-import Leadership from "./sections/Volunteering";
+import Volunteering from "./sections/Volunteering";
 import Contact from "./sections/Contact";
 import Footer from "./sections/Footer";
-import Volunteering from "./sections/Volunteering";
 
 function App() {
   return (
